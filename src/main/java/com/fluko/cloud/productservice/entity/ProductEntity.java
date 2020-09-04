cf23ce2def538b658bd20a37d3a2df2e18fa5570
Gerardo RAZO JABANA <slaarks.com>
2020-09-03 19:31:58 -0500

    Update product table name.

--- src/main/java/com/fluko/cloud/productservice/entity/ProductEntity.java
+++ src/main/java/com/fluko/cloud/productservice/entity/ProductEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "product")
+@Table(name = "tb_product")
 public class ProductEntity implements Serializable {
 	
 	private static final long serialVersionUID = -2343243243242432341L;
